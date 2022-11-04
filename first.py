42f28190676319446008c953e5278989b192e8c0
EvgeniiaIvonina <117288202+EvgeniiaIvonina@users.noreply.github.com>
2022-11-04 14:51:10 +0100

    Initial commit

---
+++ first.py
@@ -0,0 +1,61 @@
+print('Lets go')
+number = 3
+number2 = 5
+number3 = 6
+result = number + number2 + number3
+print(result)
+
+num1 = num2 = 5
+print(num1, num2)
+
+num_1, num_2 = 5, 7
+print(num_1, num_2)
+
+swap1 = 8
+swap2 = 9
+swap1, swap2 = swap2, swap1 +swap2
+print(swap1, swap2)
+
+swap2 -= number
+print(swap2)
+
+z, x, *c = [1, 2, 3, 4, 5]
+print(z)
+print(x)
+print(c)
+
+
+
+x = -5
+if x == 0:
+    print('if')
+elif x > 0:
+    print('elif')
+else:
+    print('else')
+
+x = 5
+
+if x == 0:
+    x += 1
+
+print(5/x)
+
+x = [1, 2]
+
+if x == 0:
+    x = 1
+    print('x')
+
+elif type(x) == type(5) and type(x) == type(5.5):
+    print('x')
+
+else:
+    print('x')
+    x = 1
+
+
+
+
+print(100/x)
+
